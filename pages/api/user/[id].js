40b6cef3e561c033b269c78583dae14f9ebeb3e3
Dung Duc Huynh (Kaka) <870029+jellydn@users.noreply.github.com>
2021-10-06 23:50:56 +0800

    Add params on API doc

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -1,9 +1,14 @@
-
 /**
  * @swagger
- * /api/user/[id]:
+ * /api/user/:id
  *   get:
  *     description: get user by id
+ *     parameters:
+ *       - name: id
+ *         description: User id.
+ *         in: query
+ *         required: true
+ *         type: string
  *     responses:
  *       200:
  *         description: get user by id
@@ -12,19 +17,19 @@ export default function userHandler(req, res) {
   const {
     query: { id, name },
     method,
-  } = req
+  } = req;
 
   switch (method) {
     case 'GET':
       // Get data from your database
-      res.status(200).json({ id, name: `User ${id}` })
-      break
+      res.status(200).json({ id, name: `User ${id}` });
+      break;
     case 'PUT':
       // Update or create data in your database
-      res.status(200).json({ id, name: name || `User ${id}` })
-      break
+      res.status(200).json({ id, name: name || `User ${id}` });
+      break;
     default:
-      res.setHeader('Allow', ['GET', 'PUT'])
-      res.status(405).end(`Method ${method} Not Allowed`)
+      res.setHeader('Allow', ['GET', 'PUT']);
+      res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
